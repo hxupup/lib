6e7bed6a4da051f6e4c9369ab2dfe76cf1deb646
houxu <562919529@qq.com>
2014-12-07 20:55:11 +0800

    增加通用性

--- insert_sort.h
+++ insert_sort.h
@@ -1,9 +1,21 @@
 template <typename item_type>
-void insert_sort(item_type *pItem, int n) {
+int compare(item_type a, item_type b) {
+    if (a > b) {
+        return 1;
+    } else if (a < b) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+template <typename item_type>
+void insert_sort(item_type *pItem, int n,
+        int (*pfCompare)(item_type a, item_type b) = compare<item_type>) {
     for (int i = 1; i < n; ++i) {
         item_type key = pItem[i];
         int j;
-        for (j = i - 1; j >= 0 && key < pItem[j]; --j) {
+        for (j = i - 1; j >= 0 && pfCompare(pItem[j], key) > 0; --j) {
             pItem[j + 1] = pItem[j];
         }
         pItem[j + 1] = key;
